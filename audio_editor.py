2c02e830ed457c9f1b2f54c0b74e1c3918614c35
Stepan Kuchin <stepa.ku4in@gmail.com>
2023-08-15 14:24:21 +0500

    add reverse, volume and speed

--- audio_editor.py
+++ audio_editor.py
@@ -21,22 +21,61 @@ def show_edition_commands():
     print('3   - Вырезать фрагмент')
     print('4   - Склеить фрагменты')
     print('5   - Изменить конкретную часть текущего аудио')
-    print('p   - Воспроизвести «задом наперёд»')
-    print('pr  - Воспроизвести «задом наперёд»')
-    # print('s   - Сохранить аудио')
+    print('r   - «Развернуть» аудиозапись (реверс)')
+    print('s   - Сохранить аудио')
 
 
 def read_command(commands):
     while True:
         command = input()
-        if command in commands:
+        if command.lower() in commands:
             return command
         else:
             print('Неверно введена команда')
 
 
+def _read_speed():
+    success = False
+    speed = 1.0
+    while not success:
+        try:
+            print('Введите новую скорость воспроизведения в диапозоне от 0.5 до 2.0')
+            speed = float(input())
+        except ValueError as e:
+            print('Неверно введено значение! (Попробуйте заменить «,» на «.»)')
+        else:
+            if 0.5 <= speed <= 2.0:
+                success = True
+            else:
+                print('Число вне диапозона!')
+    return speed
+
+
+def _read_volume():
+    success = False
+    volume = 1.0
+    print('Текущая громкость: 1.0')
+    while not success:
+        try:
+            print('Введите новую громкость воспроизведения (значение >= 0)')
+            volume = float(input())
+        except ValueError as e:
+            print('Неверно введено значение! (Попробуйте заменить «,» на «.»)')
+        else:
+            if volume > 0:
+                success = True
+            else:
+                print('Число вне диапозона!')
+    return volume
+
+
 class AudioEditor:
     def __init__(self):
+        self.running = False
+        self.editing = False
+        self.audio = None
+        self.output_name = outp
+
         self.menu_commands = {
             's': self.edition,
             'hs': 1,
@@ -47,12 +86,11 @@ class AudioEditor:
         self.edition_commands = {
             'm': self.back_to_menu,
             '0': self.stop,
-            '2': self.change_speed
-        }
+            '1': self.change_volume,
+            '2': self.change_speed,
 
-        self.running = False
-        self.editing = False
-        self.audio = None
+            'r': self.reverse
+        }
 
     def run(self):
         print('Добро пожаловать в аудио редактор!')
@@ -67,18 +105,37 @@ class AudioEditor:
             function = self.menu_commands[command]
             function()
 
+    def back_to_menu(self):
+        self.editing = False
+
     def stop(self):
         self.editing = False
         self.running = False
         print('Приложение успешно завершило работу')
 
-    def change_speed(self):
+    def change_volume(self):
+        volume = _read_volume()
         proc = subprocess.Popen(['ffmpeg', '-loglevel', '-8', '-i', self.audio, '-af',
-                                 'atempo=2.0', outp])
+                                 f'volume={volume}', self.output_name])
+        proc.wait()
+        print('Громкость успешно изменена\n')
+
+    def change_speed(self):
+        speed = _read_speed()
+        proc = subprocess.Popen(['ffmpeg', '-loglevel', '-8', '-i', self.audio, '-af',
+                                 f'atempo={speed}', self.output_name])
         proc.wait()
         print('Скорость успешно изменена\n')
 
+    def reverse(self):
+        proc = subprocess.Popen(['ffmpeg', '-loglevel', '-8', '-i', self.audio, '-af',
+                                 'areverse', self.output_name])
+        proc.wait()
+        print('Аудиозапись «развернута»\n')
+
     def load_audio(self):
+        self.audio = 'test.mp3'
+        return
         loaded = False
         while not loaded:
             print('Введите название аудиотрека (формат: .mp3, .wav) или напишите "m" чтобы вернуться в меню')
@@ -96,9 +153,6 @@ class AudioEditor:
                     print('Неверное разрешение аудиотрека! Попробуйте .mp3 или .wav')
             else:
                 print(f'Аудиотрек {audio} не найден!')
-
-    def back_to_menu(self):
-        self.editing = False
 
     def edition(self):
         self.editing = True
